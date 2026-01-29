3ba0c3fc22ee42a0777a8f1555818b9ab2313f65
Ersalor <mehmet.z.sanli@gmail.com>
2026-01-29 13:53:08 +0300

    feat: implement accessors for User entity

--- src/User.java
+++ src/User.java
@@ -6,4 +6,43 @@ public class User {
     private String password;
     private Role role;
 
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setSurname(String surname){
+        this.surname = surname;
+    }
+
+    public String getSurname() {
+        return surname;
+    }
+
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
+
+    public String getPhone(){
+        return phone;
+    }
+
+    public void setPassword(String password){
+        this.password = password;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setRole(Role role) {
+        this.role = role;
+    }
+
+    public Role getRole(){
+        return role;
+    }
 }
