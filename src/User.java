5488127122b36617421b7b69cf25d68a4961c5ab
Ersalor <mehmet.z.sanli@gmail.com>
2026-01-29 14:12:12 +0300

    feat: add id accessors and finalize User entity structure

--- src/User.java
+++ src/User.java
@@ -6,6 +6,14 @@ public class User {
     private String password;
     private Role role;
 
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
     public void setName(String name) {
         this.name = name;
     }
